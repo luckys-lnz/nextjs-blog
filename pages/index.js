33906d7849bf74988584eced60fa9c77290a5f30
luckys-lnz <lalucky22@gmail.com>
2023-12-29 13:24:11 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
+import Link from 'next/link';
+import Date from '../components/date';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>My name is Thomas Grahm. I am an
+          Enthusiastic and results-driven software engineer
+          with years of experience in designing and implementing scalable software solutions.
+          I am Proficient in Python,Javascript,C,C#,Reactjs,Nextjs, and SQL,Expressjs
+          programming languages.
+          I am Adept at collaborating with cross-functional teams to deliver
+          high-quality software solutions.
+          Say hi on <a href="https://twitter.com/luckystech">Twitter</a></p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
